1421fa8b932cf69e25d9a4044c365af6020823aa
hanyahunya <java020103@gmail.com>
2025-04-14 14:46:28 +0900

    [add] gitRemindプロジェクト始め

---
+++ src/main/java/com/hanyahunya/gitRemind/GitRemindApplication.java
@@ -0,0 +1,13 @@
+package com.hanyahunya.gitRemind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRemindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanyahunya/gitRemind/GitRemindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanyahunya.gitRemind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRemindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
